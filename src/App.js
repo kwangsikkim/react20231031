d3a2d9766045bb8deb0e9821ee4fe0332504b0f3
kwangsik Kim <penggusic@gmail.com>
2023-10-31 12:20:14 +0900

    component연습

--- src/App.js
+++ src/App.js
@@ -2,9 +2,44 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      {/*
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+      */}
+      <MyComponent2 />
+    {/*
+      <p>lorem ipsum adas asdasd </p>
+      <ul>
+        <li>lorem</li>
+        <li>ipsum</li>
+        <li>dolor</li>
+      </ul>
+    */}
     </div>
   );
 }
 
+function MyComponent1() {
+    return (
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+    )
+}
+
+function MyComponent2() {
+    return (
+        <>
+            {/*p>lorem5^ul>li*3>lorem1*/}
+            <p>Lorem ipsum dolor sit amet.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Labore.</li>
+                <li>Quas?</li>
+            </ul>
+        </>
+    );
+}
 export default App;
