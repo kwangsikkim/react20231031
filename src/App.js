35691ddc9439e0e8f101fd9e8cfefc4632d3e6ac
kwangsik Kim <penggusic@gmail.com>
2023-10-31 16:34:38 +0900

    인라인 스타일 적용하는 방법 연습

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-
 function App() {
   return (
-    <div>
+    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+      <MyComp />
+      <MyComp />
+      <MyComp />
+    </div>
+  );
+}
 
+function MyComp() {
+  const name = "흥민";
+  return (
+    <div
+      style={{
+        backgroundColor: "blue",
+        color: "white",
+        padding: "10px",
+        width: "25%",
+        textAlign: "center",
+      }}
+    >
+      {name}
     </div>
   );
 }
