f82d5333cb1636715f96e16ff6a54c765e3abf09
kwangsik Kim <penggusic@gmail.com>
2023-11-01 12:23:06 +0900

    하나의 파일에서 여러 component, 변수, 객체를 export

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// export 된 컴포넌트(변수, 함수, 객체 등 모두)를 사용하려면 import 해야 함
-import { MyElem } from "./components/MyElem.js";
-import { MyComp } from "./components/MyComp";
-import { MyHeader } from "./components/MyHeader";
+import { MyBox, MyElem, MyContainer, name, address, person } from "./MyBox";
 
 function App() {
+  console.log("name", name);
+  console.log("address", address);
+  console.log("person", person);
+
   return (
     <>
-      <MyHeader />
-      <MyComp />
+      <MyBox />
       <MyElem />
+      <MyContainer />
     </>
   );
 }
