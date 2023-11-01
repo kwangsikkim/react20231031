033ae57564e8a66de7f8b6f6b32c4b46c55d8f02
kwangsik Kim <penggusic@gmail.com>
2023-11-01 16:30:45 +0900

    css 모듈 사용시 여러 클래스명을 주는 방법

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -3,7 +3,12 @@ import myStyle from "../style/style2.module.css";
 export function MyContainer() {
   return (
     <div>
-      <h1 className={myStyle.boxClass}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={`${myStyle.boxClass} ${myStyle.error} ${myStyle.note}`}>
+        Lorem ipsum dolor sit amet.1
+      </h1>
+      <h1 className={[myStyle.boxClass, myStyle.error, myStyle.note].join(" ")}>
+        Lorem ipsum dolor sit amet.2
+      </h1>
     </div>
   );
 }
