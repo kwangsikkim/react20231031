fa83ccd603ac9f5e1b1088f7def424eb6f51380c
kwangsik Kim <penggusic@gmail.com>
2023-11-02 11:33:19 +0900

    simpleGrid 사용, 반응형웹 적용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
+  /*
+   base: "0em", // 0px
+   sm: "30em", // ~480px. em is a relative unit and is dependant on the font size.
+   md: "48em", // ~768px
+   lg: "62em", // ~992px
+   xl: "80em", // ~1280px
+   "2xl": "96em", // ~1536px
+  */
   return (
     <>
-      <Flex
-        w={"100%"}
-        h={"100px"}
-        bg={"pink.100"}
-        gap={3}
-        align={"center"}
-        justify={"space-around"}
-      >
-        <Box bg={"gold"}>Lorem.</Box>
-        <Box bg={"green"}>Qui.</Box>
-        <Box bg={"red"}>Exercitationem!</Box>
-        <Box bg={"blue"}>Iure?</Box>
-        <Box bg={"grey"}>Sint!</Box>
-      </Flex>
+      <SimpleGrid spacing={"10px"} columns={{ base: 2, md: 3, "2xl": 6 }}>
+        <Box bg="blue.300">Lorem.</Box>
+        <Box bg="blue.300">Repellat!</Box>
+        <Box bg="blue.300">Nesciunt?</Box>
+        <Box bg="blue.300">Reiciendis.</Box>
+        <Box bg="blue.300">Odio!</Box>
+        <Box bg="blue.300">Delectus.</Box>
+        <Box bg="blue.300">Accusantium.</Box>
+        <Box bg="blue.300">Labore!</Box>
+        <Box bg="blue.300">Laborum!</Box>
+        <Box bg="blue.300">Numquam.</Box>
+        <Box bg="blue.300">Iure?</Box>
+        <Box bg="blue.300">Nemo!</Box>
+        <Box bg="blue.300">Doloremque.</Box>
+        <Box bg="blue.300">Deserunt!</Box>
+        <Box bg="blue.300">Dolore.</Box>
+        <Box bg="blue.300">Mollitia.</Box>
+        <Box bg="blue.300">Quam.</Box>
+        <Box bg="blue.300">Impedit.</Box>
+        <Box bg="blue.300">Possimus.</Box>
+        <Box bg="blue.300">Minus!</Box>
+        <Box bg="blue.300">Quas?</Box>
+        <Box bg="blue.300">Sunt!</Box>
+        <Box bg="blue.300">Ipsa.</Box>
+        <Box bg="blue.300">Consequatur.</Box>
+        <Box bg="blue.300">Illo?</Box>
+        <Box bg="blue.300">Accusantium.</Box>
+        <Box bg="blue.300">Odit.</Box>
+        <Box bg="blue.300">Veritatis?</Box>
+        <Box bg="blue.300">Est.</Box>
+        <Box bg="blue.300">Voluptatem!</Box>
+      </SimpleGrid>
     </>
   );
 }
