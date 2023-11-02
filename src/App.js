fdd4d0675b96f06596b78b59bc53191edc053475
kwangsik Kim <penggusic@gmail.com>
2023-11-02 12:11:31 +0900

    simpleGrid 사용, 반응형웹 적용 - Card, cardheader, heading, cardfooter

--- src/App.js
+++ src/App.js
@@ -1,47 +1,380 @@
-import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  Flex,
+  SimpleGrid,
+} from "@chakra-ui/react";
 
 function App() {
-  /*
-   base: "0em", // 0px
-   sm: "30em", // ~480px. em is a relative unit and is dependant on the font size.
-   md: "48em", // ~768px
-   lg: "62em", // ~992px
-   xl: "80em", // ~1280px
-   "2xl": "96em", // ~1536px
-  */
   return (
     <>
-      <SimpleGrid spacing={"10px"} columns={{ base: 2, md: 3, "2xl": 6 }}>
-        <Box bg="blue.300">Lorem.</Box>
-        <Box bg="blue.300">Repellat!</Box>
-        <Box bg="blue.300">Nesciunt?</Box>
-        <Box bg="blue.300">Reiciendis.</Box>
-        <Box bg="blue.300">Odio!</Box>
-        <Box bg="blue.300">Delectus.</Box>
-        <Box bg="blue.300">Accusantium.</Box>
-        <Box bg="blue.300">Labore!</Box>
-        <Box bg="blue.300">Laborum!</Box>
-        <Box bg="blue.300">Numquam.</Box>
-        <Box bg="blue.300">Iure?</Box>
-        <Box bg="blue.300">Nemo!</Box>
-        <Box bg="blue.300">Doloremque.</Box>
-        <Box bg="blue.300">Deserunt!</Box>
-        <Box bg="blue.300">Dolore.</Box>
-        <Box bg="blue.300">Mollitia.</Box>
-        <Box bg="blue.300">Quam.</Box>
-        <Box bg="blue.300">Impedit.</Box>
-        <Box bg="blue.300">Possimus.</Box>
-        <Box bg="blue.300">Minus!</Box>
-        <Box bg="blue.300">Quas?</Box>
-        <Box bg="blue.300">Sunt!</Box>
-        <Box bg="blue.300">Ipsa.</Box>
-        <Box bg="blue.300">Consequatur.</Box>
-        <Box bg="blue.300">Illo?</Box>
-        <Box bg="blue.300">Accusantium.</Box>
-        <Box bg="blue.300">Odit.</Box>
-        <Box bg="blue.300">Veritatis?</Box>
-        <Box bg="blue.300">Est.</Box>
-        <Box bg="blue.300">Voluptatem!</Box>
+      <SimpleGrid gap={3} columns={{ base: 1, md: 3, xl: 6 }}>
+        {/*Card*30>CardHeader>Heading[size="md"]>lorem2^^CardBody>lorem15^CardFooter>Button>lorem1*/}
+        <Card>
+          <CardHeader>
+            <Heading size="md">Lorem ipsum.</Heading>
+          </CardHeader>
+          <CardBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+            aut eligendi impedit necessitatibus nihil obcaecati.
+          </CardBody>
+          <CardFooter>
+            <Button>Lorem.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Placeat, quae?</Heading>
+          </CardHeader>
+          <CardBody>
+            Asperiores consequuntur ducimus eligendi fugit illo, impedit ipsum
+            odit quae quam sunt tempora voluptas voluptatibus!
+          </CardBody>
+          <CardFooter>
+            <Button>Fuga.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Blanditiis, soluta?</Heading>
+          </CardHeader>
+          <CardBody>
+            Adipisci amet aperiam eum ex excepturi in iste natus officiis
+            perspiciatis repellat repudiandae, sed ullam.
+          </CardBody>
+          <CardFooter>
+            <Button>Distinctio.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Nesciunt, repellendus.</Heading>
+          </CardHeader>
+          <CardBody>
+            At blanditiis consequatur, dolore explicabo harum laboriosam maiores
+            maxime minima nesciunt quam. Cum, dicta modi!
+          </CardBody>
+          <CardFooter>
+            <Button>Earum.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Accusantium, maiores!</Heading>
+          </CardHeader>
+          <CardBody>
+            A, dolore dolorum magni nulla ratione recusandae rem repudiandae.
+            Aliquam architecto esse excepturi incidunt sunt!
+          </CardBody>
+          <CardFooter>
+            <Button>Repellat.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Est, laborum!</Heading>
+          </CardHeader>
+          <CardBody>
+            Alias amet, commodi consequatur consequuntur eligendi fugit id
+            magnam maxime nesciunt numquam possimus quisquam saepe!
+          </CardBody>
+          <CardFooter>
+            <Button>Earum.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Cum, sequi!</Heading>
+          </CardHeader>
+          <CardBody>
+            Et excepturi in incidunt magni pariatur perspiciatis quae sunt.
+            Accusantium animi asperiores doloribus maiores mollitia.
+          </CardBody>
+          <CardFooter>
+            <Button>Quo!</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Dicta, labore!</Heading>
+          </CardHeader>
+          <CardBody>
+            Amet cupiditate dolor dolorem eum exercitationem itaque, magnam,
+            molestiae nam numquam quibusdam quis saepe voluptate.
+          </CardBody>
+          <CardFooter>
+            <Button>Laudantium?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Aspernatur, ex!</Heading>
+          </CardHeader>
+          <CardBody>
+            Est fuga fugit, maiores nobis officia officiis quam quia quidem sunt
+            suscipit temporibus vel veritatis!
+          </CardBody>
+          <CardFooter>
+            <Button>Earum!</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Optio, veniam.</Heading>
+          </CardHeader>
+          <CardBody>
+            Asperiores corporis ea earum eum magni mollitia optio repudiandae
+            similique sunt, voluptatum! Debitis, eum, veritatis.
+          </CardBody>
+          <CardFooter>
+            <Button>Amet.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Maiores, vitae.</Heading>
+          </CardHeader>
+          <CardBody>
+            Asperiores iusto quibusdam quidem quod quos. Ab, excepturi, illum.
+            Deleniti, neque, ratione. Labore sequi, vel!
+          </CardBody>
+          <CardFooter>
+            <Button>Ad?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Possimus, quod.</Heading>
+          </CardHeader>
+          <CardBody>
+            Accusantium asperiores assumenda corporis excepturi id itaque modi
+            natus non omnis quae recusandae, repellendus temporibus!
+          </CardBody>
+          <CardFooter>
+            <Button>Praesentium.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Id, non.</Heading>
+          </CardHeader>
+          <CardBody>
+            Alias dolorum ea est eum fugiat itaque perferendis possimus quaerat
+            tempora tenetur veniam, voluptate voluptatum.
+          </CardBody>
+          <CardFooter>
+            <Button>Alias?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Debitis, magnam.</Heading>
+          </CardHeader>
+          <CardBody>
+            Cum ex fugiat quis voluptates? Accusamus aliquid consequuntur enim
+            maxime molestiae? Distinctio reprehenderit sed unde?
+          </CardBody>
+          <CardFooter>
+            <Button>Dolores?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Animi, consequatur!</Heading>
+          </CardHeader>
+          <CardBody>
+            Aperiam aut cum enim error labore necessitatibus non optio quam quas
+            vitae? Alias provident, suscipit.
+          </CardBody>
+          <CardFooter>
+            <Button>Aliquam?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Aut, rem?</Heading>
+          </CardHeader>
+          <CardBody>
+            Accusamus adipisci amet commodi ea harum recusandae sit ullam. Animi
+            deserunt est et facilis totam.
+          </CardBody>
+          <CardFooter>
+            <Button>Ullam!</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Ipsam, quisquam?</Heading>
+          </CardHeader>
+          <CardBody>
+            Assumenda autem cupiditate iste minima molestias nam natus officia
+            perferendis saepe similique sint, unde vel!
+          </CardBody>
+          <CardFooter>
+            <Button>Quasi?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Dolores, eaque?</Heading>
+          </CardHeader>
+          <CardBody>
+            Ab aliquam eveniet, facere id incidunt obcaecati officiis quis
+            reprehenderit, sint, tempora unde vel voluptatum.
+          </CardBody>
+          <CardFooter>
+            <Button>Id!</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Expedita, ipsa!</Heading>
+          </CardHeader>
+          <CardBody>
+            Amet at deserunt dolore eius eos eveniet iure minus, natus nisi
+            omnis quibusdam sint voluptates.
+          </CardBody>
+          <CardFooter>
+            <Button>Architecto.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Asperiores, culpa.</Heading>
+          </CardHeader>
+          <CardBody>
+            Aspernatur, aut debitis dolore mollitia nam, nulla perspiciatis quas
+            ratione sunt velit veritatis, vero voluptas!
+          </CardBody>
+          <CardFooter>
+            <Button>Laborum!</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Accusamus, beatae!</Heading>
+          </CardHeader>
+          <CardBody>
+            Accusantium iusto magnam necessitatibus vel? Amet delectus esse hic
+            odit reiciendis reprehenderit sint unde vero.
+          </CardBody>
+          <CardFooter>
+            <Button>Consectetur?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Aliquid, sint.</Heading>
+          </CardHeader>
+          <CardBody>
+            A ab accusantium adipisci aperiam asperiores consectetur corporis
+            dignissimos enim, esse labore nemo quos repellat.
+          </CardBody>
+          <CardFooter>
+            <Button>Architecto.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Maiores, repellat?</Heading>
+          </CardHeader>
+          <CardBody>
+            Consequuntur cum dignissimos dolor, dolorem doloremque eius impedit
+            iste libero necessitatibus praesentium quae repellat veritatis?
+          </CardBody>
+          <CardFooter>
+            <Button>Itaque?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Autem, rerum.</Heading>
+          </CardHeader>
+          <CardBody>
+            Aliquam aliquid cumque deserunt nulla officia, repudiandae sequi! Ad
+            cupiditate ipsum, odio quae rerum voluptas.
+          </CardBody>
+          <CardFooter>
+            <Button>Obcaecati!</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Aliquid, maxime!</Heading>
+          </CardHeader>
+          <CardBody>
+            Animi aut commodi delectus doloremque ea, enim et impedit, ipsa
+            ipsum molestias officiis quas repudiandae!
+          </CardBody>
+          <CardFooter>
+            <Button>Recusandae!</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Aspernatur, et?</Heading>
+          </CardHeader>
+          <CardBody>
+            Asperiores blanditiis corporis cupiditate deserunt dicta doloremque
+            modi molestias natus officia qui quisquam, quo veritatis.
+          </CardBody>
+          <CardFooter>
+            <Button>Ducimus?</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Ipsa, nostrum?</Heading>
+          </CardHeader>
+          <CardBody>
+            Ab beatae cumque error facere illo impedit iure nemo officiis
+            perspiciatis quod, tempore totam ullam!
+          </CardBody>
+          <CardFooter>
+            <Button>Labore.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Dignissimos, eveniet.</Heading>
+          </CardHeader>
+          <CardBody>
+            Aliquam aut cum delectus dolorem, ducimus, facere illo incidunt iste
+            molestiae quam quibusdam, repellat voluptatum!
+          </CardBody>
+          <CardFooter>
+            <Button>Rem.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Aliquid, officia?</Heading>
+          </CardHeader>
+          <CardBody>
+            Ad beatae debitis dicta dignissimos, doloribus ea error natus nihil
+            provident sed sunt suscipit, voluptas!
+          </CardBody>
+          <CardFooter>
+            <Button>Dolorum.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Facilis, quisquam!</Heading>
+          </CardHeader>
+          <CardBody>
+            Accusantium error esse, iste nam nihil nisi porro quam sint! Enim
+            explicabo hic libero quo.
+          </CardBody>
+          <CardFooter>
+            <Button>Dicta?</Button>
+          </CardFooter>
+        </Card>
       </SimpleGrid>
     </>
   );
