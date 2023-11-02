a942eb650768efb6b89d2780c16727a06af6725d
kwangsik Kim <penggusic@gmail.com>
2023-11-02 09:34:33 +0900

    필요없는 파일들 삭제함

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-// style 적용 방법
-// 보통 css 파일
-// css module 사용
-// chakraui css library
-
-import { MyComp } from "./components/MyComp";
-import { MyBox } from "./components/MyBox";
-import { MyContainer } from "./components/MyContainer";
-
 function App() {
-  return (
-    <>
-      <MyComp />
-      <MyBox />
-      <MyContainer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/MyBox.js
+++
@@ -1,9 +0,0 @@
-import myCss from "../style/style1.module.css";
-
-export function MyBox() {
-  return (
-    <div>
-      <h1 className={myCss.boxClass}>Lorem ipsum dolor sit amet.</h1>
-    </div>
-  );
-}
