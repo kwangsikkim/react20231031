565ac47fc7927f1437be288ecc4ebcc4a96a2c75
kwangsik Kim <penggusic@gmail.com>
2023-11-02 15:25:41 +0900

    Switch 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { Box, FormControl, FormLabel, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Switch,
+  Textarea,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
       <Box w="480px">
-        <FormControl>
-          <FormLabel>본문</FormLabel>
-          <Textarea placeholder="본문을 작성하세요" h={"500px"}></Textarea>
+        <FormControl display={"flex"} alignItems={"center"}>
+          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
+          <Switch size={"lg"} colorScheme="red" />
         </FormControl>
       </Box>
     </div>
