642cced72a0dab0c85213b663665b61d597b0084
kwangsik Kim <penggusic@gmail.com>
2023-11-02 15:15:27 +0900

    Select 사용

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
-import {
-  Box,
-  Center,
-  Checkbox,
-  CheckboxGroup,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-} from "@chakra-ui/react";
+import { Box, FormControl, Select } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
-      <Center w={"480px"}>
-        <Box>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-            <FormHelperText>필수 입력사항입니다.</FormHelperText>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center mt={5}>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>하나만 선택하세요</FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">Lorem.</Radio>
-                <Radio value="value2">Sed.</Radio>
-                <Radio value="value3">Ratione.</Radio>
-                <Radio value="value4">Qui.</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center>
-        <Box w="480px">
-          <FormControl>
-            <CheckboxGroup>
-              <Flex gap={5}>
-                <Checkbox colorScheme="orange">Lorem.</Checkbox>
-                <Checkbox colorScheme="green">Corporis!</Checkbox>
-                <Checkbox colorScheme="red">Saepe?</Checkbox>
-                <Checkbox colorScheme="blue">Soluta.</Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
+      <Box w="480px">
+        <FormControl>
+          <Select placeholder="선택하세요" multiple h={"120px"}>
+            <option value="value1">Lorem.</option>
+            <option value="value2">Accusamus.</option>
+            <option value="value3">Reiciendis.</option>
+            <option value="value4">Non?</option>
+            <option value="value5">Fugit.</option>
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   );
 }
