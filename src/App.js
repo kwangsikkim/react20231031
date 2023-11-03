75f26079508e57287373c97c18fe2b6f0bc0efb3
kwangsik Kim <penggusic@gmail.com>
2023-11-03 11:44:36 +0900

    이벤트 핸들러 메소드에 event 객체가 전달된다

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  // 브라우저는 이벤트 핸들러 메소드ㅇ[
-  // event 객체를 매개값으로 넣어줌
-  function handleClick(event) {
-    // console.log(event);
-    console.log(event.target);
-    // console.log(event.target.className);
-    // console.log(event.target.type);
+  function handleChange(e) {
+    // console.log("인풋값이 변경됨");
+    console.log(e.target.value);
   }
 
   return (
     <div>
-      <Button colorScheme="pink" onClick={handleClick}>
-        버튼1
-      </Button>
-      <Button colorScheme="blue" onClick={handleClick}>
-        버튼2
-      </Button>
+      <Input onChange={handleChange} />
+      <Input onChange={handleChange} />
     </div>
   );
 }
