d85a5466554e734d650101ef37122aa6ae591af3
kwangsik Kim <penggusic@gmail.com>
2023-11-06 10:29:14 +0900

    hook 사용 조건 어겨보기(if 내에 사용)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  // HOOK : use 로 시작하는 함수
-  // hook은 컴포넌트 상단에 작성해야함
-  // 즉, 조건문이나, 반복문, 중첩된 함수 안에 작성하면 안됨.
+  const [number, setNumber] = useState(0);
 
-  if (true) {
-    // hook 코드 작성 불가
-  }
-
-  for (let i = 0; i < 3; i++) {
-    // hook 코드 작성 불가
-  }
-
-  function hancle() {
-    // hook 코드 작성 불가
+  if (number > 5) {
+    const [text, setText] = useState("");
   }
 
   return <div></div>;
