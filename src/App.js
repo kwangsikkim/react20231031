f9f1b63769030bc26853a608aee45ac2d666a801
kwangsik Kim <penggusic@gmail.com>
2023-11-06 16:14:32 +0900

    use immer로 상태 변경

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Input, Text } from "@chakra-ui/react";
+import { useImmer } from "use-immer";
+
+function App(props) {
+  const [person, setPerson] = useState({ name: "son", city: "seoul" });
+  const [user, updateUser] = useImmer({ name: "lee", city: "jeju" });
+  function handleNameChange(e) {
+    const nextPerson = { ...person };
+    nextPerson.name = e.target.value;
+
+    setPerson(nextPerson);
+  }
+
+  function handleCityChange(e) {
+    const nextPerson = { ...person };
+    nextPerson.city = e.target.value;
+
+    setPerson(nextPerson);
+  }
+
+  function handelUserNameChange(e) {
+    updateUser((draft) => {
+      draft.name = e.target.value;
+    });
+  }
+
+  function handleUserCityChange(e) {
+    updateUser((draft) => {
+      draft.city = e.target.value;
+    });
+  }
+
+  return (
+    <div>
+      <Input value={person.name} onChange={handleNameChange} />
+      <Input value={person.city} onChange={handleCityChange} />
+      <Text>
+        {person.name}은 {person.city}에 산다.
+      </Text>
+
+      <hr />
+
+      <Input value={user.name} onChange={handelUserNameChange} />
+      <Input value={user.city} onChange={handleUserCityChange} />
+      <Text>
+        {user.name}은 {user.city}에 산다.
+      </Text>
+    </div>
+  );
+}
+
+export default App;
