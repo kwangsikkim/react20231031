f53578c10f9b2f6cc3fbf9ed6d89429dc527c44c
kwangsik Kim <penggusic@gmail.com>
2023-11-06 12:33:09 +0900

    배열을 상태로 쓸 경우, 상태 변경 시 새로운 배열로 복사해서 사용해야 함.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
-import React from "react";
-import { List, ListItem } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Button, Input, List, ListItem } from "@chakra-ui/react";
 
 function App(props) {
-  const arr = ["피자", "돈까스", "햄버거", "김치찌개"];
-  const arr2 = ["김승대", "고영준", "제카"];
-  const arr3 = ["라떼", "모카", "카푸치노"];
+  const [coffees, setCoffees] = useState(["라떼"]);
+  const [text, setText] = useState("");
 
-  const arr4 = [
-    { id: 0, name: "라떼" },
-    { id: 1, name: "모카" },
-    { id: 2, name: "카푸치노" },
-  ];
-  // 배열을 jsx에서 사용하기, 리스트아이템에 매핑되는 컴포넌트는 key prop을 꼭 가질 것!
-  // key prop은 유일한 값이어야 하며, 주로 db의 primary key가 사용될 것이다.
+  function handleButtonClick() {
+    // 직전 상태 배열을 조작하면 안되고,
+    // coffees.push(text);
 
-  const listItems = arr.map((item, index) => (
-    <ListItem key={index}>{item}</ListItem>
-  ));
-  // const listPh = arr2.map((ph) => <ListItem>{ph}</ListItem>);
-  // 이 위의 코드를 아래에 바로 써도 됨
+    // 새 배열을 만들어서 set state 해야 한다.
+    const newCoffees = [...coffees]; // 배열 복사
+    newCoffees.push(text);
+
+    setCoffees(newCoffees);
+    console.log(coffees);
+  }
 
   return (
     <div>
-      <List>{listItems}</List>
-      <hr />
+      <Input value={text} onChange={(e) => setText(e.target.value)} />
+      <Button onClick={handleButtonClick}>추가</Button>
+
       <List>
-        {arr2.map((ph, index) => (
-          <ListItem key={index}>{ph}</ListItem>
-        ))}
-      </List>
-      <hr />
-      <List>
-        {arr3.map((cafe, i) => (
-          <ListItem key={i}>{cafe}</ListItem>
+        {coffees.map((c, index) => (
+          <ListItem key={index}>{c}</ListItem>
         ))}
       </List>
     </div>
