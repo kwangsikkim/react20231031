1d5f9e836d64243ae338ccfa7450e2bcd24dea5d
kwangsik Kim <penggusic@gmail.com>
2023-11-07 15:48:44 +0900

    ajax get 사용 state 업데이트

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
+import axios from "axios";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
+  const [myState, setMyState] = useState("");
 
-  console.log("useEffect 밖의 로그", number); // 렌더링 될 때마다 실행
-
-  // useEffect
-  // 컴포넌트 외부 시스템과 작업할 때
-  // 주로 ajax로 데이터 가져올 때 사용
-
-  // 첫번째 파라미터 : 실행할 함수
-  // 두번째 파라미터 : 첫번째 파라미터를 실행시키는 값, 빈 배열이면 초기 렌더링 떄만 실행됨
   useEffect(() => {
-    console.log("이펙트의 첫번째 파라미터 함수 실행됨", number); // 첫 로딩때만 실행
+    axios
+      .get("/api/main1/sub1")
+      .then((response) => response.data)
+      .then((data) => setMyState(data))
+      .catch((error) => console.log(error));
   }, []);
 
-  return (
-    <div>
-      <Button onClick={() => setNumber(number + 1)}>증가</Button>
-      <Text>{number}</Text>
-    </div>
-  );
+  return <div>{myState}</div>;
 }
 
 export default App;
