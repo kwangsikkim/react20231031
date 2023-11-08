5df454fd3377a93cba00a4441e0618417b2965dc
kwangsik Kim <penggusic@gmail.com>
2023-11-08 12:28:17 +0900

    useEffect 사용 시 자주하는 실수 depencency값(trigger 하는 값)을 변경하면 안됨!!

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Box, Select, Spinner, Text } from "@chakra-ui/react";
-import axios from "axios";
 
 function App(props) {
-  const [customerIdList, setCustomerIdList] = useState([]);
-  const [customerId, setCustomerId] = useState(0);
-  const [customer, setCustomer] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [number, setNumber] = useState(0);
 
+  // useEffect 사용 시 자주하는 실수
   useEffect(() => {
-    // 고객번호들 가져오기
-    axios
-      .get("/api/main1/sub6")
-      .then((response) => setCustomerIdList(response.data));
-  }, []);
-
-  useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get("/api/main1/sub4?id=" + customerId)
-      .then((response) => response.data)
-      .then((data) => setCustomer(data))
-      .catch((error) => setCustomer(null))
-      .finally(() => setIsLoading(false));
-  }, [customerId]);
-
-  return (
-    <div>
-      <Select
-        placeholder="고객 번호를 선택하세요"
-        onChange={(e) => setCustomerId(e.target.value)}
-      >
-        {customerIdList.map((id) => (
-          <option value={id}>{id}</option>
-        ))}
-      </Select>
-      <Box>
-        {isLoading && <Spinner />}
-
-        {isLoading || (
-          <>
-            {customer === null ? (
-              <Text>조회한 고객이 없습니다. 다른 번호를 선택해주세요.</Text>
-            ) : (
-              <Text>고객 이름 : {customer.customerName}</Text>
-            )}
-          </>
-        )}
-      </Box>
-    </div>
-  );
+    console.log("코드 실행");
+    // setNumber(number + 1); // trigger 하는 값을 변경하면 안됨!!
+  }, [number]);
+  return <div></div>;
 }
 
 export default App;
